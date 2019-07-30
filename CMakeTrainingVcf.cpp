751aa161089b68ece57bc39125ecb2273d71284a
m.wittig <m.wittig@mucosa.de>
2019-07-30 13:22:59 +0200

    I would like to generate test data

---
+++ CMakeTrainingVcf.cpp
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CMakeTrainingVcf.cpp
+ * Author: mwittig
+ * 
+ * Created on July 30, 2019, 12:42 PM
+ */
+
+#include "CMakeTrainingVcf.h"
+
+/*
+ * 
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	UnnamedSample
+chr1	25747230	.	G	C	457.03	.	AC=2;AF=1.0;AN=2;DP=16;ExcessHet=3.0103;FS=0.0;MLEAC=2;MLEAF=1.0;MQ=59.69;QD=28.56;SOR=0.941	GT:AD:DP:GQ:PL	1/1:0,16:16:48:471,48,0
+
+ chr4	145041720	.	A	G	251.60	.	AC=1;AF=0.5;AN=2;BaseQRankSum=-2.915;DP=17;ExcessHet=3.0103;FS=0.0;MLEAC=1;MLEAF=0.5;MQ=60.0;MQRankSum=0.0;QD=14.8;ReadPosRankSum=-0.293;SOR=0.813	GT:AD:DP:GQ:PL:PS	0|1:7,10:17:99:259,0,193:14502904
+  
+ */
+
+CMakeTrainingVcf::CMakeTrainingVcf() 
+{
+    
+}
+
+CMakeTrainingVcf::CMakeTrainingVcf(const CMakeTrainingVcf& orig) 
+{
+    
+}
+
+CMakeTrainingVcf::~CMakeTrainingVcf() 
+{
+    
+}
+
---
+++ CMakeTrainingVcf.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   CMakeTrainingVcf.h
+ * Author: mwittig
+ *
+ * Created on July 30, 2019, 12:42 PM
+ */
+
+#ifndef CMAKETRAININGVCF_H
+#define CMAKETRAININGVCF_H
+
+class CMakeTrainingVcf {
+public:
+    CMakeTrainingVcf();
+    CMakeTrainingVcf(const CMakeTrainingVcf& orig);
+    virtual ~CMakeTrainingVcf();
+private:
+
+};
+
+#endif /* CMAKETRAININGVCF_H */
+
