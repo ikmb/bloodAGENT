f1ad2ed5a95c28e7bab6111e55d08eecf94a8e98
m.wittig <m.wittig@mucosa.de>
2019-08-05 10:45:48 +0200

    error: passing ‘const CParsedTextfile’ as ‘this’ argument discards qualifiers [-fpermissive]

--- ISBTAnno.cpp
+++ ISBTAnno.cpp
@@ -87,7 +87,11 @@ std::vector<std::string> CISBTAnno::getVariationsAt(std::string chrom, int pos)c
     mRange = m_entry_finder.equal_range(osr.str());
     
     for (multimap<string,int>::const_iterator it=mRange.first; it!=mRange.second; ++it)
-      vRet.push_back(m_vanno.cell(it->second,"Transcript annotation short"));
+    {
+        string act_entry = m_vanno.cell(it->second,"Transcript annotation short");
+        vRet.push_back(act_entry);
+    }
+    
     
     return vRet;
 }
@@ -149,7 +153,10 @@ string CISBTAnno::getSystemAt(std::string chrom, int pos)const
     mRange = m_entry_finder.equal_range(osr.str());
     
     if(mRange.first != mRange.second)
-        return m_vanno.cell(mRange.first->second,"system/gene");
+    {
+        string act_entry = m_vanno.cell(mRange.first->second,"system/gene");
+        return act_entry;
+    }
     
     
     return "";
