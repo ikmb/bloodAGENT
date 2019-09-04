bca5af6574d8b073fc3480b2e2db3a20a1423309
m.wittig <m.wittig@mucosa.de>
2019-09-04 16:52:03 +0200

    command line parsing stuff extended

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ using namespace std;
 using namespace BamTools;
 
 
-void phenotype(const string& arg_target_anno,const string& arg_isbt_SNPs,const string& arg_genotype_to_phenotype,const string& arg_vcf_file,const string& arg_bigWig,int arg_coverage, int arg_verbose);
+void phenotype(const string& arg_target_anno,const string& arg_isbt_SNPs,const string& arg_genotype_to_phenotype,const string& arg_vcf_file,const string& arg_bigWig,int arg_coverage, int arg_verbose, float arg_top_hits = 1.0, const string& arg_locus = "");
 
 /*
  * 
@@ -98,11 +98,25 @@ int main(int argc, char** argv)
             cmdjob.add(tc_bigwig);
             TCLAP::ValueArg<int> tc_coverage("c","coverage","The minimum required coverage for a solid call.",false,10,"int");
             cmdjob.add(tc_coverage);
+            TCLAP::ValueArg<float> tc_tophits("r","scoreRange","Value between 0.0 and 1.0. After genotyping multiply the best score with this value and report all genotypes better than the resulting value.",false,1.0,"int");
+            cmdjob.add(tc_tophits);
+
+            
+            // ln -s ../mnts/sftp\:host\=ikmbhead.rz.uni-kiel.de\,user\=sukko545/ifs/data/nfs_share/sukko545/haemo/DZHK/190233/190233.hg19.bwa.variants.ISBT.vcf.gz SNPs.vcf.gz
+            // ln -s ../mnts/sftp\:host\=ikmbhead.rz.uni-kiel.de\,user\=sukko545/ifs/data/nfs_share/sukko545/haemo/DZHK/190233/190233.hg19.bwa.bw coverage.bw
 
             if(tc_jobType.getValue().compare("phenotype") == 0)
             {
                 cmdjob.parse(argc,argv);
                 cerr << "Everything found. starting run ..." << endl;
+                phenotype(tc_abo_target_annotation.getValue(),
+                        tc_variants.getValue(),
+                        tc_gt2pt.getValue(),
+                        tc_vcf.getValue(),
+                        tc_bigwig.getValue(),
+                        tc_coverage.getValue(), 
+                        tc_verbose.getValue(),
+                        tc_tophits.getValue());
                 exit(EXIT_SUCCESS);
             }
             else if(tc_jobType.getValue().compare("pht") == 0)
@@ -111,7 +125,15 @@ int main(int argc, char** argv)
                 cmdjob.add(tc_system);
                 cmdjob.parse(argc,argv);
                 cerr << "Everything found. starting run ..." << endl;
-                cerr << "this job is currently not implemented" << endl;
+                phenotype(tc_abo_target_annotation.getValue(),
+                        tc_variants.getValue(),
+                        tc_gt2pt.getValue(),
+                        tc_vcf.getValue(),
+                        tc_bigwig.getValue(),
+                        tc_coverage.getValue(), 
+                        tc_verbose.getValue(),
+                        tc_tophits.getValue(),
+                        tc_system.getValue());
                 exit(EXIT_SUCCESS);
             }
         }
@@ -123,12 +145,17 @@ int main(int argc, char** argv)
     }
     catch(const TCLAP::ArgException& err)
     {
+        cerr << "Error: " << err.what() << endl;
+        exit(EXIT_FAILURE);
+    }
+    catch(const CMyException& err)
+    {
         cerr << "Error parsing command line: " << err.what() << endl;
         exit(EXIT_FAILURE);
     }
     catch(...)
     {
-        cerr << "Unexpected error parsing command line." << endl;
+        cerr << "Unexpected error." << endl;
         exit(EXIT_FAILURE);
     }
     
@@ -154,73 +181,86 @@ int main(int argc, char** argv)
 }
         
 
-void phenotype(const string& arg_target_anno,const string& arg_isbt_SNPs,const string& arg_genotype_to_phenotype,const string& arg_vcf_file,const string& arg_bigWig,int arg_coverage, int arg_verbose)
+void phenotype(const string& arg_target_anno,const string& arg_isbt_SNPs,const string& arg_genotype_to_phenotype,const string& arg_vcf_file,const string& arg_bigWig,int arg_coverage, int arg_verbose, float arg_top_hits, const string& arg_locus)
 {
-    
-    CTranscriptAnno trans_anno(arg_target_anno);
-    if(arg_verbose == 2)
-        cerr << "transcript annotation loaded from:"  << arg_target_anno << endl;
-    CISBTAnno  isbt(arg_isbt_SNPs);
-    if(arg_verbose == 2)
-        cerr << "ISBT variations loaded from:"  << arg_isbt_SNPs << endl;
-    CIsbtGt2Pt isbTyper(arg_genotype_to_phenotype);
-    if(arg_verbose == 2)
-        cerr << "ISBT genotype to phenotype translation loaded from:"  << arg_genotype_to_phenotype << endl;
-    CVcf vcf_file(arg_vcf_file);
-    if(arg_verbose == 2)
-        cerr << "VCF file loaded from:"  << arg_vcf_file << endl;
-    CBigWigReader bwr(arg_bigWig);
-    if(arg_verbose == 2)
-        cerr << "BigWig file loaded from:"  << arg_bigWig << endl;
-    
-    isbt.addCoverage(bwr);
-    std::set<string> loci = isbt.loci();
-    
-    
-    if(arg_verbose == 2)
+    try
     {
-        cerr << "ISBT annotation:" << endl << isbt << endl << endl;
-        cerr << "ISBT genotype to phenotype translation:" << endl << isbTyper << endl << endl;
-        
-     }
-    
-    // generate VCF Test Data
-    
-    /*// make test data
-    //    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/mwittig/coding/cpp/MyTools/dist/Debug/GNU-Linux/:/home/mwittig/coding/cpp/bamtools/build/src/api/:/home/mwittig/coding/cpp/htslib
-    for(auto locus:loci)
-    {
-        for(auto known_allele:isbTyper.alleleVector(locus))
-        {
-             CMyTools::cmd("cat /home/mwittig/coding/cpp/deepBlood/data/testdata/VCFheaderTestData.vcf > /home/mwittig/ramDisk/simulator.vcf",false);
-            ofstream outfile;
-            outfile.open("/home/mwittig/ramDisk/simulator.vcf", std::ios_base::app);
-            outfile << CMakeTrainingVcf::getHomEntries(locus,known_allele,isbt) << endl;
-            outfile.close();
-            /// if you analyze simulated VCF do the following on your CVariantChains object
-            // vcs.removeReferenceSnps();
-        }
-    }//*/
-    
-    CVariantChains vcs(&isbt);
-    //CVcf vcf_file(argv[3]);
-    while(vcf_file.read_record())
-    {
-        CVcfSnp act_snp = vcf_file.get_record();
-        //cerr << act_snp << endl;
-        bool adding_successfull = vcs.add(act_snp);
-        if(!adding_successfull && arg_verbose >= 1)
-            cerr << act_snp << "\tSNP not added as it is not ISBT relevant" << endl;
-        else if(adding_successfull && arg_verbose == 2)
-            cerr << act_snp << "\tISBT relevant SNP added" << endl;
-    };
+        CTranscriptAnno trans_anno(arg_target_anno);
+        if(arg_verbose == 2)
+            cerr << "transcript annotation loaded from:"  << arg_target_anno << endl;
+        CISBTAnno  isbt(arg_isbt_SNPs);
+        if(arg_verbose == 2)
+            cerr << "ISBT variations loaded from:"  << arg_isbt_SNPs << endl;
+        CIsbtGt2Pt isbTyper(arg_genotype_to_phenotype);
+        if(arg_verbose == 2)
+            cerr << "ISBT genotype to phenotype translation loaded from:"  << arg_genotype_to_phenotype << endl;
+        CVcf vcf_file(arg_vcf_file);
+        if(arg_verbose == 2)
+            cerr << "VCF file loaded from:"  << arg_vcf_file << endl;
+        CBigWigReader bwr(arg_bigWig);
+        if(arg_verbose == 2)
+            cerr << "BigWig file loaded from:"  << arg_bigWig << endl;
 
-    if(arg_verbose == 2)
-        cerr << "Variant chains of current sample: " << vcs << endl;    
-    for(auto locus:loci)
+        isbt.addCoverage(bwr);
+        std::set<string> loci = isbt.loci();
+
+
+        if(arg_verbose == 2)
+        {
+            cerr << "ISBT annotation:" << endl << isbt << endl << endl;
+            cerr << "ISBT genotype to phenotype translation:" << endl << isbTyper << endl << endl;
+
+         }
+
+        // generate VCF Test Data
+
+        /*// make test data
+        //    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/mwittig/coding/cpp/MyTools/dist/Debug/GNU-Linux/:/home/mwittig/coding/cpp/bamtools/build/src/api/:/home/mwittig/coding/cpp/htslib
+        for(auto locus:loci)
+        {
+            for(auto known_allele:isbTyper.alleleVector(locus))
+            {
+                 CMyTools::cmd("cat /home/mwittig/coding/cpp/deepBlood/data/testdata/VCFheaderTestData.vcf > /home/mwittig/ramDisk/simulator.vcf",false);
+                ofstream outfile;
+                outfile.open("/home/mwittig/ramDisk/simulator.vcf", std::ios_base::app);
+                outfile << CMakeTrainingVcf::getHomEntries(locus,known_allele,isbt) << endl;
+                outfile.close();
+                /// if you analyze simulated VCF do the following on your CVariantChains object
+                // vcs.removeReferenceSnps();
+            }
+        }//*/
+
+        CVariantChains vcs(&isbt);
+        //CVcf vcf_file(argv[3]);
+        while(vcf_file.read_record())
+        {
+            CVcfSnp act_snp = vcf_file.get_record();
+            //cerr << act_snp << endl;
+            bool adding_successfull = vcs.add(act_snp);
+            if(!adding_successfull && arg_verbose >= 1)
+                cerr << act_snp << "\tSNP not added as it is not ISBT relevant" << endl;
+            else if(adding_successfull && arg_verbose == 2)
+                cerr << act_snp << "\tISBT relevant SNP added" << endl;
+        };
+
+        if(arg_verbose == 2)
+            cerr << "Variant chains of current sample: " << vcs << endl;    
+        for(auto locus:loci)
+        {
+            if(arg_locus.length() == 0 ||  locus.compare(arg_locus) == 0)
+            {
+                isbTyper.type(locus,vcs);
+                cout << locus << '\t' << isbTyper.getCallAsString(locus,false,arg_top_hits) << endl;
+            }
+        }
+    }
+    catch(const CMyException& err)
     {
-        isbTyper.type(locus,vcs);
-        cout << locus << '\t' << isbTyper.getCallAsString(locus,true) << endl;
+        throw(err);
+    }
+    catch(...)
+    {
+        throw(CMyException("Unexpected Error in phenotype function"));
     }
 }
 
