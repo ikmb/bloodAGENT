cab12646f3573736c47a141db10ad3974f5ad9d7
m.wittig <m.wittig@mucosa.de>
2021-07-28 12:38:25 +0200

    wobble score introduced. Wobble score ignores that uncovered SNPs may withhold important information about underlying genomic variations

--- CIsbtGt2Pt.cpp
+++ CIsbtGt2Pt.cpp
@@ -267,6 +267,7 @@ nlohmann::json CIsbtGt2Pt::getJsonOfTypingResult(const CIsbtGt& gt,const std::ma
     jRet["phenotypes"]=phenotypes;
     jRet["flat_phenotypes"]=flat_phenotypes;
     jRet["score"]=getPredictedScoreOfGenotype(results);
+    jRet["wobble_score"]=jRet["score"];
     
     return jRet;
 }
@@ -311,6 +312,7 @@ nlohmann::json CIsbtGt2Pt::getCallAsJson(const CISBTAnno& isbt_anno, const CTran
                 js["phenotypes"].push_back("n.a.");
                 js["flat_phenotypes"].push_back("n.a.");
                 js["score"]=0.0f;
+                js["wobble_score"]=0.0f;
                 j["calls"].push_back(js);
                 type_by_snps = false;
             }
@@ -323,10 +325,11 @@ nlohmann::json CIsbtGt2Pt::getCallAsJson(const CISBTAnno& isbt_anno, const CTran
                 js["phenotypes"].push_back("RHD*01N.01");
                 js["flat_phenotypes"].push_back("RHD*01N.01");
                 js["score"]=2.0f;
+                js["wobble_score"]=2.0f;
                 j["calls"].push_back(js);
                 type_by_snps = false;
             }
-            else if( rhd_cov/rhce_cov <= 0.7 )
+            else if( rhd_cov/rhce_cov <= 0.66 )
             {
                 is_RHD_DEL_HET = true;
             }
